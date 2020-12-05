5a4b0464a7adaa41f7015f51f9d24f3eeed1dca7
qifanoctave <58703252+qifanoctave@users.noreply.github.com>
2020-12-05 17:35:24 +0800

    Add files via upload

--- Bucket.cpp
+++ Bucket.cpp
@@ -1,13 +1,19 @@
 #include "Bucket.hpp"
 
-Bucket::Bucket() {}
+Bucket::Bucket(Disk *disk) {
+    this->disk = disk;
+}
 
 void Bucket::add_left_rel_page(unsigned int page_id) {
     this->left_rel.push_back(page_id);
+    /* Maintain num_left_rel_record */
+    this->num_left_rel_record += this->disk->diskRead(page_id)->size();
 }
 
 void Bucket::add_right_rel_page(unsigned int page_id) {
     this->right_rel.push_back(page_id);
+    /* Maintain num_right_rel_record */
+    this->num_right_rel_record += this->disk->diskRead(page_id)->size();
 }
 
 vector<unsigned int> Bucket::get_left_rel() {
